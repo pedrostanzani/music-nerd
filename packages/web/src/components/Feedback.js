929283409c9d122638e100789a55402aa4774d06
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-11-26 07:06:55 -0300

    Disable debugging console.log from Feedback.js

--- packages/web/src/components/Feedback.js
+++ packages/web/src/components/Feedback.js
@@ -3,8 +3,6 @@ const Feedback = ({ question, isAnswered, isCorrect, selectedAnswer, handleNext 
     return;
   }
 
-  console.log(question);
-
   return (
     <div>
       <p>You selected {selectedAnswer}. That answer { isCorrect ? 'is correct' : 'is not correct' }.</p>
